fcc0f2f34748b0b79213456efb8a793fe5c6af75
josh-willy91 <jwilly91812@yahoo.com>
2021-10-27 15:06:45 -0400

    updated app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
 
 function App() {
+
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <h1>Simple Fractal</h1>
+      <h3>Benchmark Portal</h3>
+      <p>Enter your employee ID to see where you stand amongst other candidates</p>
+      <label>Employee ID</label>
+      <input
+        id = 'originInput'
+        type='text'
+        placeholder='Empoyee ID'
+        // name='origin'
+        required
+        // value={origin}
+        // onChange={updateOrigin}
+      >
+      </input>
+      <button>Submit</button>
     </div>
   );
 }
